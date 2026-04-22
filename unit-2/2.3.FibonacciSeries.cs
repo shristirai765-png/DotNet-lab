67e11443c4bd2f291240c5e6d81c03ec1e061c44
Shristi Rai <shristirai765@gmail.com>
2026-04-22 08:02:38 +0545

    fibonacci

--- unit-2/2.3.FibonacciSeries.cs
+++ unit-2/2.3.FibonacciSeries.cs
@@ -22,7 +22,7 @@
 //         }
 //         else if(n == 0)
 //         {
-//             return 0;
+//             return 0;c
 //         }
 //         else 
 //             return Fibonacci(n-1)+ Fibonacci(n-2);
